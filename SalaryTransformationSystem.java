ce5af89999ad100b6d899ce0657b37919a831e0d
KotlaMahindra11 <vtu27634@veltech.edu.in>
2026-08-05 12:04:19 +0530

    Create SalaryTransformationSystem.java

---
+++ SalaryTransformationSystem.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.stream.*;
+public class SalaryTransformation {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		List<Integer> salaries = new ArrayList<>();
+		
+		for (int i = 0; i < N; i++) {
+			salaries.add(sc.nextInt());
+		}
+		
+		salaries.stream()
+				.map(salary -> salary + (salary * 10 / 100))
+				.forEach(updatedsalary -> System.out.print(updatedsalary + " "));
+		
+		
+		
+		sc.close();
+		
+		
+	}
+
+}
